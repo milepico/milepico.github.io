ea264f96ff60ef020f6030f9659f98dcbac38aef
milepico <141552957+milepico@users.noreply.github.com>
2026-03-25 06:57:59 +0000

    Deploying to gh-pages from @ milepico/milepico.github.io@696532d80101949755dd78ec26daf9404acc5a09 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -65,7 +65,18 @@ ninja.data = [{
               handler: () => {
                 window.location.href = "/blog/";
               },
-            },{id: "post-google-gemini-updates-flash-1-5-gemma-2-and-project-astra",
+            },{id: "post-introduction-to-cuda-amp-gpu-architecture",
+        
+          title: "Introduction to CUDA &amp; GPU Architecture",
+        
+        description: "《CUDA编程：基础与实践》阅读笔记",
+        section: "Posts",
+        handler: () => {
+          
+            window.location.href = "/blog/2026/CUDA_Basics/";
+          
+        },
+      },{id: "post-google-gemini-updates-flash-1-5-gemma-2-and-project-astra",
         
           title: 'Google Gemini updates: Flash 1.5, Gemma 2 and Project Astra <svg width="1.2rem" height="1.2rem" top=".5rem" viewBox="0 0 40 40" xmlns="http://www.w3.org/2000/svg"><path d="M17 13.5v6H5v-12h6m3-3h6v6m0-6-9 9" class="icon_svg-stroke" stroke="#999" stroke-width="1.5" fill="none" fill-rule="evenodd" stroke-linecap="round" stroke-linejoin="round"></path></svg>',
         
